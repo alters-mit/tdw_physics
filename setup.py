03b42504347f40954e5bbe17f9750e65930553c7
alters <alters@mit.edu>
2020-05-18 12:14:12 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tdw',
+    version="0.1",
+    description='Generic structure to create physics datasets with TDW.',
+    long_description="Required Python scripts for TDW.",
+    url='https://github.com/alters-mit/tdw_physics',
+    author='Seth Alter',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    keywords='unity simulation tdw hdf5',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['tqdm', 'numpy', 'h5py', 'tdw'],
+)
