5c5931fc7d8e5265fb3f539943694fa1c33050bd
alters-mit <alters@mit.edu>
2020-05-28 09:49:48 -0400

    cleanup

--- controllers/shadows.py
+++ controllers/shadows.py
@@ -10,29 +10,31 @@ from tdw_physics.util import get_args
 
 class _Sector:
     """
-    A zone with interesting lighting.
+    A sector has two points.
+
+    A "sub-sector" is the circle defined by one of these points and RADIUS.
     """
 
     RADIUS = 0.5
 
     def __init__(self, c_0: Dict[str, float], c_1: Dict[str, float]):
         """
-        :param c_0: Center of the "from" subsector.
-        :param c_1: Center of the "to" subsector.
+        :param c_0: Center of a subsector.
+        :param c_1: Center of a subsector.
         """
         self.c_0 = TDWUtils.vector3_to_array(c_0)
         self.c_1 = TDWUtils.vector3_to_array(c_1)
 
     def get_p_0(self) -> Dict[str, float]:
         """
-        :return: A position in the c_0 sector.
+        :return: A position in the c_0 sub-sector.
         """
 
         return TDWUtils.array_to_vector3(TDWUtils.get_random_point_in_circle(center=self.c_0, radius=self.RADIUS))
 
     def get_p_1(self) -> Dict[str, float]:
         """
-        :return: A position in the c_1 sector.
+        :return: A position in the c_1 sub-sector.
         """
 
         return TDWUtils.array_to_vector3(TDWUtils.get_random_point_in_circle(center=self.c_1, radius=self.RADIUS))
@@ -40,7 +42,9 @@ class _Sector:
 
 class Shadows(RigidbodiesDataset):
     """
-    Move a ball to and from shadowed areas.
+    Move a ball to and from areas with different lighting.
+
+    Use HDRI skyboxes and visual materials (for the ball) to change the lighting per trial.
     """
 
     _BALL_SCALE = 0.3
@@ -49,6 +53,7 @@ class Shadows(RigidbodiesDataset):
                        "marble_griotte", "plastic_stripes", "plastic_vinyl_glossy_green", "plastic_vinyl_glossy_blue",
                        "plastic_vinyl_glossy_orange"]
 
+    # These sectors have different lighting at each point, e.g. c_0 is more shadowed than c_1.
     SECTORS = [_Sector(c_0={"x": 0.5, "y": 0, "z": 0}, c_1={"x": -0.5, "y": 0, "z": 0}),
                _Sector(c_0={"x": 0, "y": 0, "z": 3}, c_1={"x": -1.2, "y": 0, "z": 3.7}),
                _Sector(c_0={"x": 0, "y": 0, "z": -3}, c_1={"x": -1.2, "y": 0, "z": -3.7}),
@@ -60,11 +65,15 @@ class Shadows(RigidbodiesDataset):
     def __init__(self, port: int = 1071):
         super().__init__(port=port)
 
+        # Cache the ball data.
         self._ball = ModelLibrarian("models_flex.json").get_record("sphere")
         self._ball_id = 0
+
+        # The position the ball starts in and the position the ball is directed at.
         self._p0: Dict[str, float] = {}
         self._p1: Dict[str, float] = {}
 
+        # Cache the skybox records.
         skybox_lib = HDRISkyboxLibrarian()
         self._skyboxes: List[str] = [r.name for r in skybox_lib.records if r.sun_intensity >= 0.8]
 
@@ -84,7 +93,7 @@ class Shadows(RigidbodiesDataset):
     def get_trial_initialization_commands(self) -> List[dict]:
         # Select a random sector.
         sector: _Sector = random.choice(self.SECTORS)
-        # Decide which way the ball will go.
+        # Decide where the ball will go and in which direction.
         if random.random() < 0.5:
             self._p0 = sector.get_p_0()
             self._p1 = sector.get_p_1()
@@ -103,6 +112,7 @@ class Shadows(RigidbodiesDataset):
                                                 dynamic_friction=random.uniform(0, 0.1),
                                                 static_friction=random.uniform(0, 0.1),
                                                 bounciness=random.uniform(0, 0.1)))
+        # Scale the ball and apply a force and a spin.
         commands.extend([{"$type": "scale_object",
                           "scale_factor": {"x": self._BALL_SCALE, "y": self._BALL_SCALE, "z": self._BALL_SCALE},
                           "id": self._ball_id},
@@ -123,12 +133,14 @@ class Shadows(RigidbodiesDataset):
         # Set a random material.
         commands.extend(TDWUtils.set_visual_material(self, self._ball.substructure, self._ball_id,
                                                      random.choice(self._BALL_MATERIALS)))
-        d0 = TDWUtils.get_distance(self._p0, self._p1)
-        # Get a random skybox.
+
+        # Set a random skybox and rotate it for variable lighting.
         commands.extend([self.get_add_hdri_skybox(skybox_name=random.choice(self._skyboxes)),
                          {"$type": "rotate_hdri_skybox_by",
                           "angle": random.uniform(0, 360)}])
+
         # Teleport the avatar such that it can see both points.
+        d0 = TDWUtils.get_distance(self._p0, self._p1)
         p_med = np.array([(self._p0["x"] + self._p1["x"]) / 2, 0, (self._p0["z"] + self._p1["z"]) / 2])
         p_cen = np.array([0, 0, 0])
         a_pos = p_med + ((p_cen - p_med) / np.abs(np.linalg.norm(p_cen - p_med)) * (d0 + random.uniform(-0.1, 0.1)))
@@ -150,6 +162,7 @@ class Shadows(RigidbodiesDataset):
     def is_done(self, resp: List[bytes], frame: int) -> bool:
         for r in resp[:-1]:
             r_id = OutputData.get_data_type_id(r)
+            # If the ball reaches or overshoots the destination, the trial is done.
             if r_id == "tran":
                 t = Transforms(r)
                 d0 = TDWUtils.get_distance(TDWUtils.array_to_vector3(t.get_position(0)), self._p0)
