38b342137267cf22026a44a31c54ac2f83122939
alters-mit <alters@mit.edu>
2020-06-04 11:33:38 -0400

    sorta works...

--- controllers/drag.py
+++ controllers/drag.py
@@ -9,22 +9,64 @@ from tdw_physics.util import get_args
 
 
 class Drag(ClothDataset):
+    def get_scene_initialization_commands(self) -> List[dict]:
+        return [self.get_add_scene(scene_name="tdw_room_2018"),
+                {"$type": "set_aperture",
+                 "aperture": 3.0},
+                {"$type": "set_focus_distance",
+                 "focus_distance": 2.25},
+                {"$type": "set_post_exposure",
+                 "post_exposure": 0.4},
+                {"$type": "set_ambient_occlusion_intensity",
+                 "intensity": 0.25},
+                {"$type": "set_ambient_occlusion_thickness_modifier",
+                 "thickness": 4.0},
+                {"$type": "create_flex_container",
+                 "collision_distance": 0.001,
+                 "static_friction": 1.0,
+                 "dynamic_friction": 1.0,
+                 "iteration_count": 5,
+                 "substep_count": 8,
+                 "radius": 0.1875,
+                 "damping": 0,
+                 "drag": 0}]
+
     def get_trial_initialization_commands(self) -> List[dict]:
         commands = []
         # Add another object.
         commands.extend(self.add_cloth_object(record=self.cloth_record,
-                                              position={"x": 0, "y": 0.01, "z": 0},
+                                              position={"x": 0, "y": 1, "z": 0},
                                               rotation=TDWUtils.VECTOR3_ZERO,
                                               o_id=self.cloth_id,
-                                              mass_scale=1,
+                                              mass_scale=10,
                                               mesh_tesselation=1,
                                               tether_stiffness=random.uniform(0.5, 1.0),
                                               bend_stiffness=random.uniform(0.5, 1.0),
                                               stretch_stiffness=random.uniform(0.5, 1.0)))
-        # Let the objects settle.
+        # Let the cloth settle.
         # Position and aim avatar.
-        commands.extend([{"$type": "step_physics",
-                         "frames": 10},
+        commands.extend([{"$type": "set_kinematic_state",
+                          "id": self.cloth_id,
+                          "is_kinematic": True,
+                          "use_gravity": False},
+                         {"$type": "step_physics",
+                         "frames": 100}])
+        # Add the other object.
+        o_id = self.get_unique_id()
+        commands.extend(self.add_solid_object(record=random.choice(self.object_records),
+                                              position={"x": 0, "y": 0.2, "z": 0},
+                                              rotation={"x": 0.0, "y": random.uniform(0, 360.0), "z": 0.0},
+                                              mass_scale=1,
+                                              particle_spacing=0.035,
+                                              o_id=o_id))
+        # Let the object settle.
+        # Position and aim the camera.
+        commands.extend([{"$type": "set_kinematic_state",
+                          "id": o_id,
+                          "is_kinematic": True,
+                          "use_gravity": False},
+                         {"$type": "step_physics",
+                          "frames": 100},
                          {"$type": "teleport_avatar_to",
                           "position": self.get_random_avatar_position(2, 2.3, 1, 1.3, TDWUtils.VECTOR3_ZERO)},
                          {"$type": "look_at",
@@ -36,16 +78,6 @@ class Drag(ClothDataset):
         commands = super().get_per_frame_commands(resp, frame)
         # Apply a force.
         if frame == 1:
-            o_id = self.get_unique_id()
-            commands.extend(self.add_solid_object(record=random.choice(self.object_records),
-                                                  position={"x": 0, "y": 0.02, "z": 0},
-                                                  rotation={"x": 0.0, "y": random.uniform(0, 360.0), "z": 0.0},
-                                                  mass_scale=1000,
-                                                  particle_spacing=0.035,
-                                                  o_id=o_id))
-            commands.append({"$type": "set_kinematic_state",
-                             "id": o_id,
-                             "is_kinematic": False})
             for r in resp[:-1]:
                 if FlexParticles.get_data_type_id(r) == "flex":
                     fp = FlexParticles(r)
@@ -54,7 +86,7 @@ class Drag(ClothDataset):
                     for p in fp.get_particles(0):
                         if p[2] < 0.5 or p[0] < 0.5:
                             pos = np.array(p[:-1])
-                            force = ((pos - center) / np.linalg.norm(pos - center)) * -30
+                            force = ((pos - center) / np.linalg.norm(pos - center)) * 300
                             forces.extend(force)
                             forces.append(p[3])
 
--- tdw_physics/cloth_dataset.py
+++ tdw_physics/cloth_dataset.py
@@ -21,28 +21,6 @@ class ClothDataset(FlexDataset, ABC):
 
         super().__init__(port=port)
 
-    def get_scene_initialization_commands(self) -> List[dict]:
-        return [self.get_add_scene(scene_name="tdw_room_2018"),
-                {"$type": "set_aperture",
-                 "aperture": 3.0},
-                {"$type": "set_focus_distance",
-                 "focus_distance": 2.25},
-                {"$type": "set_post_exposure",
-                 "post_exposure": 0.4},
-                {"$type": "set_ambient_occlusion_intensity",
-                 "intensity": 0.25},
-                {"$type": "set_ambient_occlusion_thickness_modifier",
-                 "thickness": 4.0},
-                {"$type": "create_flex_container",
-                 "collision_distance": 0.001,
-                 "static_friction": 1.0,
-                 "dynamic_friction": 1.0,
-                 "iteration_count": 12,
-                 "substep_count": 12,
-                 "radius": 0.1875,
-                 "damping": 0,
-                 "drag": 0}]
-
     def get_field_of_view(self) -> float:
         return 65
 
