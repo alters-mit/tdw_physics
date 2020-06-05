0c946361beb075dca6c2d3d4ca440cc1e98aff9f
alters-mit <alters@mit.edu>
2020-06-05 14:47:58 -0400

    much better?

--- controllers/drag.py
+++ controllers/drag.py
@@ -9,6 +9,17 @@ from tdw_physics.util import get_args
 
 
 class Drag(ClothDataset):
+    def __init__(self, port: int = 1071):
+        super().__init__(port=port)
+        # The number of frames during which a force will be applied.
+        self._num_force_frames: int = 0
+        # The axis of the force (0=x, 2=z).
+        self._force_axis: int = 0
+        # Forces will only be applied to particles with coordinate values greater/lesser than this.
+        self._force_range: float = 0
+        # The force magnitude per frame.
+        self._force_per_frame: float = 0
+
     def get_scene_initialization_commands(self) -> List[dict]:
         return [self.get_add_scene(scene_name="tdw_room_2018"),
                 {"$type": "set_aperture",
@@ -33,17 +44,24 @@ class Drag(ClothDataset):
                  "drag": 0}]
 
     def get_trial_initialization_commands(self) -> List[dict]:
+        self._num_force_frames = random.randint(5, 10)
+        self._force_axis = 0 if random.random() < 0.5 else 2
+        self._force_range: float = random.uniform(0.5, 0.8)
+        self._force_per_frame = random.uniform(-30, -45)
+        if random.random():
+            self._force_range *= -1
+            self._force_per_frame *= -1
         commands = []
-        # Add another object.
+        # Add the cloth object.
         commands.extend(self.add_cloth_object(record=self.cloth_record,
                                               position={"x": 0, "y": 1, "z": 0},
                                               rotation=TDWUtils.VECTOR3_ZERO,
                                               o_id=self.cloth_id,
                                               mass_scale=10,
                                               mesh_tesselation=1,
-                                              tether_stiffness=0.5,
-                                              bend_stiffness=1,
-                                              stretch_stiffness=0.5))
+                                              tether_stiffness=random.uniform(0.5, 1),
+                                              bend_stiffness=random.uniform(0.5, 1),
+                                              stretch_stiffness=random.uniform(0.5, 1)))
         # Let the cloth settle.
         # Position and aim avatar.
         commands.extend([{"$type": "set_kinematic_state",
@@ -57,7 +75,7 @@ class Drag(ClothDataset):
         commands.extend(self.add_solid_object(record=random.choice(self.object_records),
                                               position={"x": 0, "y": 0.2, "z": 0},
                                               rotation={"x": 0.0, "y": random.uniform(0, 360.0), "z": 0.0},
-                                              mass_scale=1,
+                                              mass_scale=random.uniform(0.25, 1),
                                               particle_spacing=0.035,
                                               o_id=o_id))
         # Let the object settle.
@@ -78,18 +96,22 @@ class Drag(ClothDataset):
     def get_per_frame_commands(self, resp: List[bytes], frame) -> List[dict]:
         commands = super().get_per_frame_commands(resp, frame)
         # Apply a force.
-        if 20 >= frame == 1:
+        if frame < self._num_force_frames:
+            print(self._force_per_frame, self._force_axis, self._force_range)
             for r in resp[:-1]:
                 if FlexParticles.get_data_type_id(r) == "flex":
                     fp = FlexParticles(r)
                     forces = []
                     center = np.array(([0, 0, 0]))
+                    p_id = 0
                     for p in fp.get_particles(0):
-                        if p[2] < 0.5 or p[0] < 0.5:
+                        if (0 > self._force_range > p[self._force_axis]) or \
+                                (0 < self._force_range < p[self._force_axis]):
                             pos = np.array(p[:-1])
-                            force = ((pos - center) / np.linalg.norm(pos - center)) * 300
+                            force = ((pos - center) / np.linalg.norm(pos - center)) * self._force_per_frame
                             forces.extend(force)
-                            forces.append(p[3])
+                            forces.append(p_id)
+                        p_id += 1
 
                     forces = np.array(forces, dtype=np.float32)
                     forces = base64.b64encode(forces)
