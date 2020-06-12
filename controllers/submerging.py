1217b185c491c51fa54351f6a08d89a9438aa020
instbldrjems <jschwartz@wireframe-world.com>
2020-06-12 11:21:58 -0400

    Update submerging.py

--- controllers/submerging.py
+++ controllers/submerging.py
@@ -21,50 +21,44 @@ class Submerge(TransformsDataset):
 
     def __init__(self):
         self.model_list = [
-                      #"b03_db_apps_tech_08_04",
-                      #"trashbin",
-                      #"trunck",
-                      #"whirlpool_akzm7630ix",
-                      #"satiro_sculpture",
-                      #"towel-radiator-2",
-                      #"b03_folding-screen-panel-room-divider",
-                      #"naughtone_pinch_stool_chair",
-                      #"microwave",
-                      #"trunk_6810-0009",
+                      "b03_db_apps_tech_08_04",
+                      "trashbin",
+                      "trunck",
+                      "whirlpool_akzm7630ix",
+                      "satiro_sculpture",
+                      "towel-radiator-2",
+                      "b03_folding-screen-panel-room-divider",
+                      "naughtone_pinch_stool_chair",
+                      "microwave",
+                      "trunk_6810-0009",
                       "suitcase",
-                      #"kayak_small",
-                      #"elephant_bowl",
-                      #"trapezoidal_table",
-                      #"b05_pc-computer-printer-1",
-                      #"dishwasher_4",
-                      #"chista_slice_of_teak_table",
-                      #"buddah",
-                      #"b05_elsafe_infinity_ii",
-                      #"backpack",
-                      #"b06_firehydrant_lod0",
-                      #"b05_ticketmachine",
-                      #"b05_trophy",
-                      #"b05_kitchen_aid_toster",
-                      #"b05_heavybag",
-                      #"bongo_drum_hr_blend",
-                      #"b03_worldglobe",
-                      #"ceramic_pot",
-                      #"b04_kenmore_refr_70419",
-                      #"b03_zebra",
-                      #"b05_gibson_j-45",
-                      #"b03_cow",
-                      #"b03_sheep",
-                      #"b04_stringer"
+                      "kayak_small",
+                      "elephant_bowl",
+                      "trapezoidal_table",
+                      "b05_pc-computer-printer-1",
+                      "dishwasher_4",
+                      "chista_slice_of_teak_table",
+                      "buddah",
+                      "b05_elsafe_infinity_ii",
+                      "backpack",
+                      "b06_firehydrant_lod0",
+                      "b05_ticketmachine",
+                      "b05_trophy",
+                      "b05_kitchen_aid_toster",
+                      "b05_heavybag",
+                      "bongo_drum_hr_blend",
+                      "b03_worldglobe",
+                      "ceramic_pot",
+                      "b04_kenmore_refr_70419",
+                      "b03_zebra",
+                      "b05_gibson_j-45",
+                      "b03_cow",
+                      "b03_sheep",
+                      "b04_stringer"
                      ]
 
         self.fluid_id = None
         self.pool_id = None
-
-        ft = FluidTypes()
-        self.fluid_types = ft.get_fluid_types()
-        self.fluid_type_names = ft.get_fluid_type_names()
-
-        self.fluid_type_selection = "water"
 
         self.special_lib = ModelLibrarian("models_special.json")
         self.full_lib = ModelLibrarian("models_full.json")
@@ -75,11 +69,6 @@ class Submerge(TransformsDataset):
     def get_scene_initialization_commands(self) -> List[dict]:
         if system() != "Windows":
             raise Exception("Flex fluids are only supported in Windows (see Documentation/misc_frontend/flex.md)")
-
-        
-
-        # Randomly select a fluid type
-        #self.fluid_type_selection = choice(self.fluid_types.fluid_type_names)
 
         commands = [self.get_add_scene(scene_name="tdw_room_2018"),
                     {"$type": "set_aperture",
@@ -92,28 +81,9 @@ class Submerge(TransformsDataset):
                      "intensity": 0.175},
                     {"$type": "set_ambient_occlusion_thickness_modifier",
                      "thickness": 3.5}]
-
-        # Create the container, set up for fluids.
-        # Slow down physics so the water can settle without splashing out of the container.
-        commands.extend([{"$type": "create_flex_container",
-                          "collision_distance": 0.04,
-                          "static_friction": 0.1,
-                          "dynamic_friction": 0.1,
-                          "particle_friction": 0.1,
-                          "viscocity": self.fluid_types[self.fluid_type_selection].viscosity,
-                          "adhesion": self.fluid_types[self.fluid_type_selection].adhesion,
-                          "cohesion": self.fluid_types[self.fluid_type_selection].cohesion,
-                          "radius": 0.1,
-                          "fluid_rest": 0.05,
-                          "damping": 0.01,
-                          "substep_count": 5,
-                          "iteration_count": 8,
-                          "buoyancy": 1.0},
-                         {"$type": "set_time_step", 
-                         "time_step": 0.005}
-                        ])
  
         return commands
+
 
     def get_trial_initialization_commands(self) -> List[dict]:
         trial_commands = []
@@ -136,14 +106,40 @@ class Submerge(TransformsDataset):
 
         # Destroy the previous fluid object, if is has been assigned.
         if self.fluid_id != None:
-            trial_commands.append({"$type": "destroy_flex_object", "id": self.fluid_id})
+            trial_commands.extend([{"$type": "destroy_flex_object", "id": self.fluid_id},
+                                   {"$type": "destroy_flex_container", "id": 0}])
+
+        ft = FluidTypes()
+
+        # Randomly select a fluid type
+        fluid_type_selection = choice(ft.fluid_type_names)
+
+        # Create the container, set up for fluids.
+        # Slow down physics so the water can settle without splashing out of the container.
+        trial_commands.extend([{"$type": "create_flex_container",
+                                "collision_distance": 0.04,
+                                "static_friction": 0.1,
+                                "dynamic_friction": 0.1,
+                                "particle_friction": 0.1,
+                                "viscocity": ft.fluid_types[fluid_type_selection].viscosity,
+                                "adhesion": ft.fluid_types[fluid_type_selection].adhesion,
+                                "cohesion": ft.fluid_types[fluid_type_selection].cohesion,
+                                "radius": 0.1,
+                                "fluid_rest": 0.05,
+                                "damping": 0.01,
+                                "substep_count": 5,
+                                "iteration_count": 8,
+                                "buoyancy": 1.0},
+                               {"$type": "set_time_step", 
+                                "time_step": 0.005}
+                        ])
             
         # Select an object at random.
         model = choice(self.model_list)
         model_record = self.full_lib.get_record(model)
 
         # Recreate fluid.
-        trial_commands.extend(self.create_fluid())
+        trial_commands.extend(self.create_fluid(fluid_type_selection))
 
         # Randomly select an object, and randomly orient it.
         # Set the object to kinematic.
@@ -180,7 +176,7 @@ class Submerge(TransformsDataset):
         return trial_commands
 
 
-    def create_fluid(self) -> List[dict]:
+    def create_fluid(self, fluid_type_selection: str) -> List[dict]:
         # Add the fluid actor, using the FluidPrimitive. Allow 500 frames 
         #  for the fluid to settle before continuing.
         fluid_id = Controller.get_unique_id()
@@ -198,7 +194,7 @@ class Submerge(TransformsDataset):
                            "id": fluid_id,
                            "container_id": 0, 
                            "fluid_container": True,
-                           "fluid_type": self.fluid_type_selection},
+                           "fluid_type": fluid_type_selection},
                          {"$type": "step_physics", "frames": 500}]
 
         return command
