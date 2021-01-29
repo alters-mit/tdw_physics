9026d0767324ce730709615df4b1153946a058b6
alters-mit <alters@mit.edu>
2021-01-29 12:23:35 -0500

    Updated scene names

--- controllers/occlusion.py
+++ controllers/occlusion.py
@@ -28,7 +28,7 @@ class Occlusion(TransformsDataset):
         super().__init__(port=port)
 
     def get_scene_initialization_commands(self) -> List[dict]:
-        return [self.get_add_scene(scene_name="tdw_room_2018"),
+        return [self.get_add_scene(scene_name="tdw_room"),
                 {"$type": "set_aperture",
                  "aperture": 3.0},
                 {"$type": "set_focus_distance",
